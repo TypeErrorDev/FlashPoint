a3060c2887c8c9fbfa75392aadf8e452cdb42571
Matthew Pantel <79867801+TypeErrorDev@users.noreply.github.com>
2024-11-08 22:40:08 -0800

    Created two buttons on /settings

--- flashpoint/src/Components/Settings.jsx
+++ flashpoint/src/Components/Settings.jsx
@@ -1,7 +1,7 @@
 // ---------------------------
 // Required imports
 // ---------------------------
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 // ---------------------------
 // Import components
@@ -13,11 +13,26 @@ import Nav from "./Nav";
 // ---------------------------
 
 const Settings = () => {
+  const navigate = useNavigate();
+  const navigateToDashboard = () => {
+    console.log("Navigating to /dashbord from /settings");
+    navigate("/dashboard");
+  };
   return (
     <div>
       <Nav />
-      <div className="flex flex-col items-center text-white">
-        <h1 className="text-4xl">Settings</h1>
+
+      <div className="flex flex-col justify-center items-center text-white mt-[-125px]">
+        <h1 className="text-4xl">Settings Page</h1>
+        <button className="bg-[#444da1] text-white py-2 px-6 rounded-lg shadow-lg shadow-[#2e274e] transition-transform transform hover:scale-105 focus:outline-none focus:ring-4 focus:ring[#7d8ec7]/50 w-32 h-32 mt-20">
+          Delete All Decks
+        </button>
+        <button
+          className="bg-[#444da1] text-white py-2 px-6 rounded-lg shadow-lg shadow-[#2e274e] transition-transform transform hover:scale-105 focus:outline-none focus:ring-4 focus:ring[#7d8ec7]/50 w-32 h-32 mt-20"
+          onClick={navigateToDashboard}
+        >
+          Exit
+        </button>
       </div>
     </div>
   );
